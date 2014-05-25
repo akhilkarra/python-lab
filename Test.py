a1aed71d0de10aae91c6db6df3413dd936635c47
akhilkarra <akhil.karra@me.com>
2014-05-25 16:53:26 +0530

    First Python Program

--- Test.py
+++ Test.py
@@ -5,4 +5,10 @@ Created on 25-May-2014
 '''
 
 if __name__ == '__main__':
-    print 'Hello World'
+    """print 'Hello World"""
+    
+    starter = raw_input("What would you like for starters ?")
+    main = raw_input("What would you like for main course ?")
+    print "So, you want %s for starters and %s for the main course." % (starter, main)
+    print "Thank you very much for your order."
+    
