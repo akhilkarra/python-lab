92201ccfb090858c7eed49a50387d79646175f73
akhilkarra <akhil.karra@me.com>
2014-05-26 17:08:37 +0530

    Added BooleanOps

--- Order.py
+++ Order.py
@@ -4,9 +4,7 @@ Created on 25-May-2014
 @author: akhilkarra
 '''
 
-if __name__ == '__main__':
-    """print 'Hello World"""
-    
+if __name__ == '__main__':   
     starter = raw_input("What would you like for starters ?")
     main = raw_input("What would you like for main course ?")
     print "So, you want %s for starters and %s for the main course." % (starter, main)
