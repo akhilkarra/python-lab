76800789e3cb00cf5ef62dbc82359c4b91f8e8cb
akhilkarra <akhil.karra@me.com>
2014-06-01 13:55:23 +0530

    Cricket Attax

--- Cricket_Attax.py
+++ Cricket_Attax.py
@@ -3,16 +3,16 @@ Created on 31-May-2014
 
 @author: akhilkarra
 '''
-if __name__ == '__main__':
-    class CricketPlayer:
-        'Class to represent Cricket player in cricket attax game'
-        name = ""
-        team = ""
-        bowling = 0
-        batting = 0
-        stars = 0
-        runs = 0
-        points = 0
+
+class CricketPlayer:
+    'Class to represent Cricket player in cricket attax game'
+    name = " "
+    team = " "
+    bowling = 0
+    batting = 0
+    stars = 0
+    runs = 0
+    points = 0
 
     def __init__(self, name, team, bowling, batting, stars, runs, points):
         self.name = name
@@ -22,12 +22,39 @@ if __name__ == '__main__':
         self.stars = stars
         self.runs = runs
         self.points = points
+   
+    def print_stars(self):
+        i = 0
+        while(i < self.stars):
+            print("*"),
+            i = i + 1 
+                  
+    def display(self):
+        print "________________________"
+        print "  " , self.team, " ", self.name
+        print
+        print
+        print
+        print
+        print
+        print "         " , self.runs
+        print "        " , "runs"
+        print "  " , self.bowling, "          " , self.batting
+        print "bowling" "       batting" 
+        print "     " ,
+        self.print_stars()
+        print
+        print "         ", self.points
+        print "________________________"
         
-    def display():
-        print "name %s, team %s, bowling %s, batting %s, stars %s, runs %s, point %s" % (name, team, bowling, batting, stars, runs, points)
-            
-            
-
-
     
+        
+            
+viratKolhi = CricketPlayer("Virat Kohli", "RCB", 27, 87, 5, 32, 114)
+viratKolhi.display()
     
+dinesh = CricketPlayer("Dinesh Karthik", "DD", 10, 92, 5, 30, 166)
+dinesh.display()
+
+kaul = CricketPlayer("Siddarth Kaul", "DD", 57, 29, 2, 7, 36)
+kaul.display()
