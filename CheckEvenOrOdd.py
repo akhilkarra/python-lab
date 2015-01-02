bdc2a59ac2f830e3cf1013be306d2be366ecfa97
Akhil Karra <akhil.karra@me.com>
2015-01-02 20:17:13 +0530

    2 new files I made using Python.

---
+++ CheckEvenOrOdd.py
@@ -0,0 +1,7 @@
+x = raw_input('Pick a Number ')
+
+if int(x) % 2 == 0:
+	print 'The number is even'
+else:
+	print 'The number is odd'
+print 'Done using the conditional method...'
---
+++ SwapIntegers.py
@@ -0,0 +1,5 @@
+x,y = 2,3
+x,y = y,x
+print 'x =', x
+print 'y =', y
+print 'Program has ended'
