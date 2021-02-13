a5a1dd19edefd618a82ecdd860f03a3faf4bae11
akhilkarra <akhil.karra@me.com>
2021-02-12 22:50:02 -0500

    Fix imports

--- tests/test_flip_4_digits.py
+++ tests/test_flip_4_digits.py
@@ -1,32 +1,19 @@
+from aops.lib import InvalidNumberError, flip4digit
 import pytest
-
-from aops.lib import InvalidNumberError, flip_4_digit
 
 
 @pytest.mark.parametrize(
     "n,expected",
-    [
-        (1728, 8271),
-        (2400, 42),
-        (2004, 4002),
-        (1000, 1),
-        (9999, 9999)
-    ],
+    [(1728, 8271), (2400, 42), (2004, 4002), (1000, 1), (9999, 9999)],
 )
 def test_flip_4_digit(n: int, expected: int) -> None:
-    assert flip_4_digit(n) == expected
+    assert flip4digit(n) == expected
 
 
 @pytest.mark.parametrize(
     "n",
-    {
-        (-1),
-        (-100),
-        10,
-        245,
-        95863
-    },
+    {(-1), (-100), 10, 245, 95863},
 )
 def test_invalid_flip_4_digits(n: int) -> None:
     with pytest.raises(InvalidNumberError):
-        flip_4_digit(n)
+        flip4digit(n)
